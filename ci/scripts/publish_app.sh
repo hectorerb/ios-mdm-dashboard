1f72c5b8c0aebbcc956fd069b68eef6204fd99b5
Hector Rondon <hrondon@teclib.com>
2018-09-21 10:26:02 -0400

    ci(release): run release on fork repo
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- ci/scripts/publish_app.sh
+++ ci/scripts/publish_app.sh
@@ -4,7 +4,5 @@ if [[ $GITHUB_COMMIT_MESSAGE != *"ci(release): generate CHANGELOG.md for version
 
     # Update app info
     source "${SCRIPT_PATH}/app_info.sh"
-    # Send app to App Store with fastlane 
-    bundle exec fastlane publish
 
 if
